7a3d1ef3727409ce8a7db574fbf6bfebd0f67dc1
Wahsheerah <kelvinceogee001@gmail.com>
2025-10-21 17:09:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  const menuToggle = document.getElementById("menu-toggle");
+  const navLinks = document.querySelector("nav ul");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+</script>
